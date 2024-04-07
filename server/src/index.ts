24df8bf40777edbdde370573d2bc710ee1584fc5
Markus Neuhoff <5299121+neuhoffm@users.noreply.github.com>
2024-04-07 11:20:37 -0400

    cleaning up xml parser registration

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,17 +15,10 @@ const PORT = parseInt(process.env.PORT, 10);
 const app = express();
 
 app.use(express.json());
-app.use(
-  xmlparser({
-    explicitArray: false,
-    normalizeTags: false,
-    explicitRoot: false,
-  })
-);
+app.use(xmlparser({ explicitArray: false, explicitRoot: false }));
 
 // register routes
 app.use("/", routes);
-
 
 // register middleware
 app.use(express.static("public"));
