94048afe210041e985197828d2a5638c5c5d40a2
Markus Neuhoff <5299121+neuhoffm@users.noreply.github.com>
2024-04-16 23:42:51 -0400

    fixing scope checks

--- server/src/middleware/auth0.middleware.ts
+++ server/src/middleware/auth0.middleware.ts
@@ -1,5 +1,10 @@
 import * as dotenv from "dotenv";
-import { auth, requiredScopes } from "express-oauth2-jwt-bearer";
+import { Request, Response, NextFunction } from "express";
+import {
+  InsufficientScopeError,
+  auth,
+  claimCheck,
+} from "express-oauth2-jwt-bearer";
 
 dotenv.config();
 
@@ -8,5 +13,20 @@ export const validateAccessToken = auth({
   audience: process.env.AUTH0_AUDIENCE,
 });
 
-export const checkRequiredScope = (requiredScope: string) =>
-  requiredScopes(requiredScope);
+export const checkRequiredScope = (requiredScope: string) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const permissionCheck = claimCheck((payload) => {
+      const permissions = payload.permissions as string[];
+
+      const hasPermissions = permissions.includes(requiredScope);
+
+      if (!hasPermissions) {
+        throw new InsufficientScopeError();
+      }
+
+      return hasPermissions;
+    });
+
+    permissionCheck(req, res, next);
+  };
+};
