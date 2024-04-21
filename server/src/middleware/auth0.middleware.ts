d77cc8460259180cd6e32b7fa36ae954efe21fb7
Markus Neuhoff <5299121+neuhoffm@users.noreply.github.com>
2024-04-21 10:36:57 -0400

    using required scopes for authorization

--- server/src/middleware/auth0.middleware.ts
+++ server/src/middleware/auth0.middleware.ts
@@ -1,10 +1,5 @@
 import * as dotenv from "dotenv";
-import { Request, Response, NextFunction } from "express";
-import {
-  InsufficientScopeError,
-  auth,
-  claimCheck,
-} from "express-oauth2-jwt-bearer";
+import { auth, requiredScopes } from "express-oauth2-jwt-bearer";
 
 dotenv.config();
 
@@ -13,20 +8,5 @@ export const validateAccessToken = auth({
   audience: process.env.AUTH0_AUDIENCE,
 });
 
-export const checkRequiredScope = (requiredScope: string) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    const permissionCheck = claimCheck((payload) => {
-      const permissions = payload.permissions as string[];
-
-      const hasPermissions = permissions.includes(requiredScope);
-
-      if (!hasPermissions) {
-        throw new InsufficientScopeError();
-      }
-
-      return hasPermissions;
-    });
-
-    permissionCheck(req, res, next);
-  };
-};
+export const checkRequiredScope = (requiredScope: string) =>
+  requiredScopes(requiredScope);
