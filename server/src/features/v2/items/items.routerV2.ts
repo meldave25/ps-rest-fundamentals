2bb6e64358635f112ff658a5e2eb20b1e008535c
Markus Neuhoff <5299121+neuhoffm@users.noreply.github.com>
2024-04-27 23:05:11 -0400

    adding caching code to v2

--- server/src/features/v2/items/items.routerV2.ts
+++ server/src/features/v2/items/items.routerV2.ts
@@ -5,7 +5,11 @@ import {
   getItemDetail,
   getItems,
 } from "./items.serviceV2";
-import { idNumberRequestSchema, itemDTORequestSchema } from "../../v1/types";
+import {
+  idNumberRequestSchema,
+  itemPOSTRequestSchema,
+  itemPUTRequestSchema,
+} from "../../v1/types";
 import { validate } from "../../../middleware/validation.middleware";
 import { create } from "xmlbuilder2";
 import {
@@ -13,7 +17,7 @@ import {
   SecurityPermissions,
 } from "../../../config/permissions";
 import {
-  checkRequiredPermission,
+  checkRequiredScope,
   validateAccessToken,
 } from "../../../middleware/auth0.middleware";
 
@@ -50,17 +54,23 @@ itemsRouterV2.get("/:id", validate(idNumberRequestSchema), async (req, res) => {
       res.json(item);
     }
   } else {
-    res.status(404).json({ message: "Item Not Found" });
+    if (req.headers["accept"] == "application/xml") {
+      res
+        .status(404)
+        .send(create().ele("error", { message: "Item Not Found" }).end());
+    } else {
+      res.status(404).json({ message: "Item Not Found" });
+    }
   }
 });
 
 itemsRouterV2.post(
   "/",
   validateAccessToken,
-  checkRequiredPermission(ItemsPermissions.Write),
-  validate(itemDTORequestSchema),
+  checkRequiredScope(ItemsPermissions.Create),
+  validate(itemPOSTRequestSchema),
   async (req, res) => {
-    const data = itemDTORequestSchema.parse(req);
+    const data = itemPOSTRequestSchema.parse(req);
     const item = await upsertItem(data.body);
     if (item != null) {
       res.status(201).json(item);
@@ -73,7 +83,7 @@ itemsRouterV2.post(
 itemsRouterV2.delete(
   "/:id",
   validateAccessToken,
-  checkRequiredPermission(SecurityPermissions.Deny),
+  checkRequiredScope(SecurityPermissions.Deny),
   validate(idNumberRequestSchema),
   async (req, res) => {
     const data = idNumberRequestSchema.parse(req);
@@ -87,13 +97,13 @@ itemsRouterV2.delete(
 );
 
 itemsRouterV2.put(
-  "/",
+  "/:id",
   validateAccessToken,
-  checkRequiredPermission(ItemsPermissions.Write),
-  validate(itemDTORequestSchema),
+  checkRequiredScope(ItemsPermissions.Write),
+  validate(itemPUTRequestSchema),
   async (req, res) => {
-    const data = itemDTORequestSchema.parse(req);
-    const item = await upsertItem(data.body);
+    const data = itemPUTRequestSchema.parse(req);
+    const item = await upsertItem(data.body, data.params.id);
     if (item != null) {
       res.json(item);
     } else {
--- server/src/features/v2/items/items.serviceV2.ts
+++ server/src/features/v2/items/items.serviceV2.ts
@@ -2,38 +2,72 @@ import { PrismaClient } from "@prisma/client";
 import { ItemDTO } from "../../v1/types";
 import { Item, ItemDetail } from "../types";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import TTLCache from "@isaacs/ttlcache";
+
+const options = { ttl: 1000 * 60 * 60 * 24 };
+const cache = new TTLCache(options);
+const listKey = "items-list";
 
 const prisma = new PrismaClient();
 
 export function getItems(): Promise<Item[]> {
-  return prisma.item.findMany({
-    select: {
-      id: true,
-      name: true,
-    },
-  });
+  const cacheItems = cache.get<Item[]>(listKey);
+  if (cacheItems != undefined) {
+    return Promise.resolve(cacheItems);
+  }
+
+  return prisma.item
+    .findMany({
+      select: {
+        id: true,
+        name: true,
+      },
+    })
+    .then((items) => {
+      cache.set(listKey, items);
+      return items;
+    });
 }
 
 export function getItemDetail(itemId: number): Promise<ItemDetail | null> {
-  return prisma.item.findFirst({
-    where: { id: itemId },
-  });
+  const cacheItem = cache.get<ItemDetail>(itemId);
+  if (cacheItem != undefined) {
+    return Promise.resolve(cacheItem);
+  }
+
+  return prisma.item
+    .findFirst({
+      where: { id: itemId },
+    })
+    .then((item) => {
+      cache.set(itemId, item);
+      return item;
+    });
 }
 
-export function upsertItem(item: ItemDTO): Promise<Item | null> {
-  return prisma.item.upsert({
-    where: {
-      id: item.id || -1,
-    },
-    update: {
-      name: item.name,
-      description: item.description,
-    },
-    create: {
-      name: item.name,
-      description: item.description,
-    },
-  });
+export function upsertItem(
+  item: ItemDTO,
+  itemId?: number | null
+): Promise<Item | null> {
+  return prisma.item
+    .upsert({
+      where: {
+        id: itemId || -1,
+      },
+      update: {
+        name: item.name,
+        description: item.description,
+      },
+      create: {
+        name: item.name,
+        description: item.description,
+      },
+    })
+    .then((item) => {
+      cache.set(item.id, item);
+      cache.delete(listKey);
+      return item;
+    });
 }
 
 export function deleteItem(itemId: number): Promise<Item | null> {
@@ -50,5 +84,13 @@ export function deleteItem(itemId: number): Promise<Item | null> {
       } else {
         throw error;
       }
+    })
+    .then((item) => {
+      if (item != null) {
+        cache.delete(item.id);
+        cache.delete(listKey);
+      }
+
+      return item;
     });
 }
